6de771125033e042c2bc82f2e3645f771da10f5b
brainfucksec <brainfuck@protonmail.ch>
2018-05-27 12:00:36 +0200

    Installation file

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# rbackup installation file
+#
+# install - install a program, script, or datafile
+#
+# Copyright (C) 2018 Brainfuck
+#
+# This script is compatible with the BSD install script, but was written
+# from scratch.  It can only install one file at a time, a restriction
+# shared with many OS's install programs.
+
+
+set -euo pipefail
+
+PROG_NAME="rbackup"
+VERSION="0.1.0"
+PROG_DIR="$HOME/bin"
+DATA_DIR="$HOME/.config"
+
+
+mkdir -pv "$DATA_DIR/$PROG_NAME"
+install -Dm644 -v data/* "$DATA_DIR/$PROG_NAME"
+install -Dm755 -v rbackup.sh "$PROG_DIR/$PROG_NAME"
+
